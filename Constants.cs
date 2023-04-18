6b10cdaa72d9ec6e74b524119ee2eebac0ef99d8
Akkuuu <66589856+Akkuuu@users.noreply.github.com>
2023-04-18 14:32:44 -0700

    Create Constants.cs

---
+++ Constants.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignatureProject
+{
+    public class Constants
+    {
+        public static string AppId = "YOUR-APPID";
+        public static string TenantId = "YOUR-TENANTID";
+        public static string ClientSecret = "YOUR-CLIENTSECRET";
+        public static string TenantName = "YOUR-TENANTNAME";
+        public static string ConnectionString = "YOUR-CONNECTION-STRING";
+    }
+}
